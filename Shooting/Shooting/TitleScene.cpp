a7ca6acc8ab8bf5b354a7e93876206120e3576c8
shoes235 <shoes235@outlook.kr>
2026-09-10 11:22:26 +0900

    docs : TitleScene - 텍스쳐 위치변경 업데이트로

--- Shooting/Shooting/TitleScene.cpp
+++ Shooting/Shooting/TitleScene.cpp
@@ -4,7 +4,6 @@ int TitleScene::Init()
 {
 	IScene::Init();
 
-	Randomizer randSys;
 
 	VEC2 zero = { 0,0 };
 	_textures.emplace_back("rsc/Babem.png",zero);
@@ -12,23 +11,26 @@ int TitleScene::Init()
 	_textures.emplace_back("rsc/images.jpg",zero);
 	_textures.emplace_back("rsc/very-silly-cat-silly.png",zero);
 
-	for (auto& texture : _textures)
-	{
-		VEC2 rnPos;
-		rnPos.x = randSys.Rand(0, _winSize.cx);
-		rnPos.y = randSys.Rand(0, _winSize.cy);
+	for (auto& texture : _textures)	texture.Load();
 
-		texture.SetPos(rnPos);
-		texture.Load();
-	}
 	return 0;
 }
 
 int TitleScene::Render()
 {
 	IScene::Render();
+	
+	Randomizer randSys;
 
-	for (auto& texture : _textures) texture.Print();
+	for (auto& texture : _textures)
+	{
+		VEC2 rnPos;
+		rnPos.x = randSys.Rand(0, _winSize.cx);
+		rnPos.y = randSys.Rand(0, _winSize.cy);
+		texture.SetPos(rnPos);
+
+		texture.Print();
+	}
 
 	return 0;
 }
