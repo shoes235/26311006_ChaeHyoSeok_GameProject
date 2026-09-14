3820f64bbfba3877a7ef5b823b80baf1242ebbd1
shoes235 <shoes235@outlook.kr>
2026-09-14 15:13:56 +0900

    comment : 오류상황 해결 메뉴얼 코드

--- Shooting/Shooting/_main.cpp
+++ Shooting/Shooting/_main.cpp
@@ -25,6 +25,7 @@ SceneManager g_SceneMgr;
 //Life Cycle
 void main()
 {
+	std::cout << " \"애플리케이션 제어 정책에서 이 파일을 차단했습니다\" 문구가 뜬다면 \n [방법 1.]윈도우 보안에서 스마트 앱 컨트롤 끔으로 설정\n [방법 2.]Windows 보안의 '바이러스 및 위협 방지' 설정에서 차단된 파일을 제외 목록에 추가\n";
 	g_SceneMgr.InitEngine();
 
 	TitleScene* title = new TitleScene();
