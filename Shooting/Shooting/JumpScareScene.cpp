a592e9594ca206bc661ead526c250a4db26b9244
shoes235 <shoes235@outlook.kr>
2026-09-19 13:56:18 +0900

    feat : 점프스케어 씬

---
+++ Shooting/Shooting/JumpScareScene.cpp
@@ -0,0 +1,41 @@
+#include "JumpScareScene.h"
+
+void JumpScareScene::Init()
+{
+	SetFullScreen();
+
+	_img->Load();
+	
+	_sfx = g2_SoundLoad("rsc/audio/SFX/JumpScare.mp3");
+	
+	return;
+}
+
+void JumpScareScene::Update()
+{
+	float dt = g_deltaTimer.GetDeltaTime();
+	_timer += dt;
+
+	if (_timer >= JUMP_SCARE_TIME)
+	{
+		_trigger = true;
+		g2_SoundPlay(_sfx);
+
+	}
+
+	if (_timer >=
+		JUMP_SCARE_TIME + SCENE_CHANGE_TIME)
+	{
+		//g_SceneMgr.ChangeScene()
+	}
+
+		return;
+}
+
+void JumpScareScene::Render()
+{
+	if(_trigger)
+		_img->Print();
+
+	return;
+}
---
+++ Shooting/Shooting/JumpScareScene.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "IScene.h"
+#include "Texture.h"
+
+class JumpScareScene : public IScene
+{
+private:
+	float _timer = .0f;
+
+	bool _trigger = false;
+
+	const float JUMP_SCARE_TIME = 2.0f;
+	const float SCENE_CHANGE_TIME = 2.0f;
+
+	Texture* _img = new Texture("rsc/imgs/Cats/UnCanny.png", VEC2(300, 0));
+
+	int _sfx = 0;
+
+public:
+	JumpScareScene() : IScene(E_SceneType::JUMP_SCARE)
+	{
+		_winSize = 
+		{
+			GetSystemMetrics(SM_CXSCREEN),
+			GetSystemMetrics(SM_CYSCREEN)
+		};
+
+	}
+
+	void Init() override;
+	void Update() override;
+	void Render() override;
+
+};
+
